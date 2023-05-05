39eb0e5c2e8dfd7987ef1d1071effd2117f1bc29
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-05-05 23:34:50 +0530

    dynamic component create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactory, ComponentFactoryResolver, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { AlertComponent } from './alert/alert.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<template #alertContainer></template>
+  <button (click)="createComponent('success')">Create success alert</button>
+  <button (Click)="createComponent('danger')">Create danger alert</button>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dyanamically-component';
+ //viewchild decorator use to get referance to the templete element
+  @ViewChild("alertContainer",{read: ViewContainerRef}) container: any;
+  componentRef!: ComponentRef<any>;
+  title: any;
+
+  constructor(private resolver:ComponentFactoryResolver) {
+    
+  }
+
+  createComponent(type: any){
+    //when create a component and we need to remove it we use this clear()
+    this.container.clear();
+    //this resloveComponentFactory() method takes a component and returns to the componentFactory
+    const ComponentFactory =this.resolver.resolveComponentFactory(AlertComponent);
+    //create() method used by container to display componentRef
+    this.componentRef=this.container.createComponent(ComponentFactory);
+    // reference to our new component, and we can set the type Input
+    this.componentRef.instance.type=type;
+    //subscribe to a component Output like this
+    this.componentRef.instance.output.subscribe((event: any)=>console.log(event));
+  }
+//destory the newly create component
+  ngOnDestroy(){
+    this.componentRef.destroy();
+  }
 }
